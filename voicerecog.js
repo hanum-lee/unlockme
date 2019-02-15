b31333d596c0f3c57e257d232f8ad167127e1e8b
hanum-lee <chococloudy@gmail.com>
2019-02-15 16:42:34 -0700

    Basic structure

---
+++ voicerecog.js
@@ -0,0 +1,15 @@
+var UserDictation = artyom.newDictation({
+    continuous:true, // Enable continuous if HTTPS connection
+    onResult:function(text){
+        // Do something with the text
+        console.log(text);
+    },
+    onStart:function(){
+        console.log("Dictation started by the user");
+    },
+    onEnd:function(){
+        alert("Dictation stopped by the user");
+    }
+});
+$(document).onload(UserDictation.start());
+//UserDictation.start();
