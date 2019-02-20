29812bc7567257feee817cd60af3590ad37c446d
Hadiya <hadiya.firdaus@gmail.com>
2019-02-19 23:13:58 -0700

    Added library, and logic for speech recognition, show engl if true, show another if false

--- voicerecog.js
+++ voicerecog.js
@@ -1,12 +1,55 @@
-if (annyang) {
-    // Let's define a command.
-    var commands = {
-        'hello': function() { alert('Hello world!'); }
+window.onload = function () {
+
+    var prompt = document.getElementById('prompt');
+    var result = document.getElementById('result');
+
+    var dict = {
+        "aller": "to go",
+        "avoir": "to have",
+        "être": "to be",
+        "faire": "to make",
+        "voir": "to see",
+        "dire": "to say",
+        "falloir": "to be necessary",
+        "savoir": "to know",
     };
 
-    // Add our commands to annyang
-    annyang.addCommands(commands);
+    var setTestWord = function () {
+        var keys = Object.keys(dict);
+        prompt.textContent = keys[Math.trunc(keys.length * Math.random())];
+    };
 
-    // Start listening.
-    annyang.start();
+    setTestWord();
+
+    var openPhone = function () {
+    annyang.abort();
+    // implement, it should wait so the person sees the english word and then it fades and then we go to the new word
 }
+
+    var ifWordDisplayedAndSaid = function (spokenWord) {
+        if (prompt.textContent.localeCompare(spokenWord) == 0) {
+            prompt.textContent = "";
+            result.textContent = dict[spokenWord];
+            openPhone();
+        } else {
+            setTestWord();
+        }
+    };
+
+    
+    if (annyang) {
+        // Let's define a command.
+        var commands = {
+            '*word': ifWordDisplayedAndSaid,
+        };
+
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+
+        annyang.setLanguage("fr-FR");
+        
+        annyang.start();
+    }
+};
+
+
